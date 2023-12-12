52755fd1c5af70f83e86bdc161cbc0d48055de8c
Kai Aragaki <adamaragaki@gmail.com>
2023-12-12 08:58:13 -0500

    work on side

--- R/present.R
+++ R/present.R
@@ -11,10 +11,17 @@ vec_to_grob_heights <- function(vec) {
     Reduce(unit.c, x = _)
 }
 
-make_layout <- function(dims) {
+get_layout <- function(wb) {
+  dims <- get_dimensions(wb)
   grid.layout(
     2, 2,
     widths = unit.c(dims$side$w, dims$blot$w),
     heights = unit.c(dims$top$h, dims$blot$h)
   )
+}
+
+present_wb <- function(wb) {
+  frame <- frameGrob(layout = get_layout(wb))
+  side <- make_side(wb)
+
 }
--- R/present_side.R
+++ R/present_side.R
@@ -9,7 +9,10 @@ get_side_w <- function(wb, extra = grid::unit(0.2, "lines")) {
   ra <- row_annot(wb)
   ca <- col_annot(wb)
   all_text_vec <- c(ra$name, colnames(ca))
-  max <- lapply(all_text_vec, \(x) grid::grobWidth(grid::textGrob(x))) |>
+  max <- lapply(
+    all_text_vec,
+    \(x) grid::grobWidth(grid::textGrob(x))
+  ) |>
     Reduce(grid::unit.c, x = _) |>
     max()
   max + extra
@@ -21,4 +24,27 @@ get_side_h <- function(wb) {
 
 get_side_hs <- function(wb) {
   get_blot_hs(wb)
+}
+
+get_side_layout <- function(wb) {
+  grid::grid.layout(
+    nrow = length(row_annot(wb)$name),
+    ncol = 1,
+    heights = get_side_hs(wb),
+    widths = get_side_w(wb)
+  )
+}
+
+make_side <- function(wb) {
+  names <- row_annot(wb)$name
+  frame <- frameGrob(layout = get_side_layout(wb))
+  for (i in seq_along(names)) {
+    frame <- grid::placeGrob(
+      frame, grid::rectGrob(), row = i, col = 1
+    )
+    frame <- grid::placeGrob(
+      frame, grid::textGrob(names[i]), row = i, col = 1
+    )
+  }
+  frame
 }

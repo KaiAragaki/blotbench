cb761c93a73fa6125c15df4253176be4fa18ec38
Kai Aragaki <adamaragaki@gmail.com>
2023-12-21 20:06:14 -0700

    closes #9

---
+++ tests/testthat/test-row_annot.R
@@ -0,0 +1,28 @@
+test_that("multiplication works", {
+  expect_equal(2 * 2, 4)
+})
+
+
+test_that("row_annot is well-formed", {
+  wb <- wb(
+    c(magick::logo, magick::logo),
+    col_annot = data.frame(cond = "drug"),
+    row_annot = c("one", "two")
+  )
+  expect_error(row_annot(wb) <- "one")
+  expect_no_error(row_annot(wb) <- c("one", "two"))
+  expect_error(row_annot(wb) <- data.frame(wrong_name = c("one", "two")))
+  expect_no_error(row_annot(wb) <- data.frame(name = c("one", "two")))
+  expect_error(
+    row_annot(wb) <- data.frame(name = c("one", "two"), extra_col = "other")
+  )
+})
+
+test_that("Just row_annot NULL is not ok", {
+  expect_no_error(
+    wb <- wb(magick::logo, col_annot = NULL, row_annot = NULL)
+  )
+  expect_error(
+    wb <- wb(magick::logo, col_annot = data.frame(cond = "x"), row_annot = NULL)
+  )
+})
